80e8277b331b843cfee7396ad8756b79372991cc
student <student@nw46142.local>
2019-03-13 20:49:34 -0500

    Initial Commit

---
+++ programmingContest/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  programmingContest
+//
+//  Created by student on 3/13/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ programmingContest/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  programmingContest
+//
+//  Created by student on 3/13/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
